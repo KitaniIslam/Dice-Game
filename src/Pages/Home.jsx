cc847ea43c3940ba60229800ea0dd6dbec86d55a
kitani islam <slmkitani@gmail.com>
2021-07-30 13:37:26 +0100

    :lipstick: HomePage> center elements

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,21 @@ import Settings from "../Components/Settings"
 
 const Home = () => {
   return (
-    <div>
+    <div style={style.homeContainer}>
       <Settings />
     </div>
   )
+}
+
+const style = {
+  homeContainer: {
+    'width': '100vw',
+    'height': '100vh',
+    'display': 'flex',
+    'flex': 1,
+    'alignItems': 'center',
+    'justifyContent': 'center'
+  }
 }
 
 export default Home

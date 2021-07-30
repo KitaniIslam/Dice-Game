16b68c66f91031c3cb2092ad6622da8d8fa3d1b7
kitani islam <slmkitani@gmail.com>
2021-07-30 15:28:09 +0100

    :no_entry: redirect to homePage if te game notCreatedYet

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useHistory } from "react-router-dom"
+
 const Game = () => {
+  const { gameConfigObject, isTheGameCreated } = useSelector(state => state.game)
+  const history = useHistory()
+
+  useEffect(() => {
+    if (!isTheGameCreated) {
+      // if user try to jump directly to the game before setting up names and score
+      history.replace('/')
+    }
+  })
   return (
     <div>
-      <p>game page</p>
+      <p>{gameConfigObject.firstPlayerName}</p>
+      <p>{gameConfigObject.secondPlayerName}</p>
+      <p>{gameConfigObject.maxScore}</p>
     </div>
   )
 }

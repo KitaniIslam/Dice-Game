d844ac241a6dbafa8ddc29155cd1564deaba2e2b
kitani islam <slmkitani@gmail.com>
2021-07-31 11:04:53 +0100

    :sparkles: Player Component

---
+++ src/Components/Player.jsx
@@ -0,0 +1,78 @@
+import Button from "./Button"
+
+const Player = ({ playerName,playerId, action, totalScore, gainedScore, myTurn,showLastAddedScore }) => {
+
+  return (
+    <div className="player" style={style.player}>
+      <div style={style.centerElements}>
+        <p>{playerName}</p>
+        <div style={{ ...style.activeIndicator, 'backgroundColor': myTurn ? '#F00D5F' : '#7c7a7a' }} />
+      </div>
+      <div style={style.centerElements}>
+        <div style={myTurn ? style.scoreIndicatorActive : style.scoreIndicatorInactive}>
+          <p>{totalScore}</p>
+          {showLastAddedScore && <div style={style.addToScore}>+{gainedScore}</div>}
+        </div>
+        <Button text="Run" active={myTurn} action={action} playerId={playerId}/>
+      </div>
+    </div>
+  )
+}
+
+const style = {
+  player: {
+    'display': 'flex',
+    'flexDirection': 'column',
+    'alignItems': 'center',
+    'justifyContent': 'space-between',
+    'width': 'fit-content',
+    'padding': '2rem 4rem',
+    'height': '280px',
+    'backgroundColor': '#FBFBFB'
+  },
+  activeIndicator: {
+    'width': '8px',
+    'height': '8px',
+    'borderRadius': '50%',
+    'margin': '0.5rem 0 0 0'
+  },
+  scoreIndicatorActive: {
+    'width': '60px',
+    'height': '60px',
+    'borderRadius': '50%',
+    'display': 'flex',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+    'position': 'relative',
+    'backgroundColor': '#F00D5F',
+    'color': '#ffffff',
+  },
+  scoreIndicatorInactive: {
+    'width': '60px',
+    'height': '60px',
+    'borderRadius': '50%',
+    'display': 'flex',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+    'position': 'relative',
+    'border': 'solid 0.5px #000000',
+  },
+  centerElements: {
+    'display': 'flex',
+    'flexDirection': 'column',
+    'alignItems': 'center',
+    'justifyContent': 'center',
+  },
+  addToScore: {
+    'position': 'absolute',
+    'top': '-50%',
+    'right': '0',
+    'backgroundColor': '#F00D5F',
+    'borderRadius': '50%',
+    'fontSize': '0.8rem',
+    'padding': '0.3rem'
+
+  }
+}
+
+export default Player

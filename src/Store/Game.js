86a344d606e13bb8fa4903da5bbb1aacf516f3da
kitani islam <slmkitani@gmail.com>
2021-07-31 12:22:57 +0100

    :sparkles: GameStore> check for winner and prevent play on rolling

--- src/Store/Game.js
+++ src/Store/Game.js
@@ -4,6 +4,7 @@ export const game = createSlice({
   name: 'game',
   initialState: {
     isTheGameCreated: false,
+    allowedToPlay: false,
     gameConfigObject: {
       firstPlayerName: 'Player 1',
       secondPlayerName: 'Player 2',
@@ -21,7 +22,10 @@ export const game = createSlice({
     },
     lastAddedScore: 0,
     mostResentPlayer:0,
-    winner: null
+    winner: {
+      gameEnded: false,
+      winnerName: null
+    }
   },
   reducers: {
     setGameConfiguration: (state, action) => {
@@ -45,8 +49,10 @@ export const game = createSlice({
           break;
       }
       state.mostResentPlayer = randomPlayerId
+      state.allowedToPlay = true
     },
     setRandomBonus(state, action) {
+      state.allowedToPlay = false
       const randomScore = Math.floor(Math.random() * 6) + 1
       state.lastAddedScore = randomScore
       state.mostResentPlayer= action.payload.player
@@ -61,8 +67,9 @@ export const game = createSlice({
           state.secondPlayer = {...state.secondPlayer, showLastAddedScore: true, totalScore: state.secondPlayer.totalScore + lastAddedScore}
           break;
       }
+      game.caseReducers.checkWinner(state)
     },
-    switchPlayer(state, action) {
+    switchPlayer(state) {
       switch (state.mostResentPlayer) {
         case 1:
           state.firstPlayer = {...state.firstPlayer, myTurn: false, showLastAddedScore: false}
@@ -73,11 +80,20 @@ export const game = createSlice({
           state.firstPlayer = {...state.firstPlayer, myTurn: true, showLastAddedScore: false}
           break;
       }
+      state.allowedToPlay = true
+    },
+    checkWinner(state) {
+      if (state.firstPlayer.totalScore >= state.gameConfigObject.maxScore) {
+        state.winner = {...state.winner, gameEnded: true, winnerName: state.gameConfigObject.firstPlayerName}
+      }
+      if (state.secondPlayer.totalScore >= state.gameConfigObject.maxScore) {
+        state.winner = {...state.winner, gameEnded: true, winnerName: state.gameConfigObject.secondPlayerName}
+      }
     },
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { setGameConfiguration, selectPlayerToStart, switchPlayer, setRandomBonus, allowDisplayBonus } = game.actions
+export const { setGameConfiguration, selectPlayerToStart, switchPlayer, setRandomBonus, allowDisplayBonus, checkWinner } = game.actions
 
 export default game.reducer

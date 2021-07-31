7e0dcf062894bedb618bb94b28c70e353d228e08
kitani islam <slmkitani@gmail.com>
2021-08-01 00:16:56 +0100

    :zap: Modal> restartTheGame onNewGameClick button

--- src/Components/modal/ResetRestartModal.jsx
+++ src/Components/modal/ResetRestartModal.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-import { freezeTheGame } from '../../Store/Game'
+import { freezeTheGame, restartTheGame } from '../../Store/Game'
 import './style.css'
 import Button from '../Button'
 
@@ -8,7 +8,9 @@ import Button from '../Button'
 const ResetRestartModal = () => {
   const { gameEnded, winnerName } = useSelector(state => state.game.winner)
   const dispatch = useDispatch()
-  const handleClose = () => { }
+  const resetTheGame = () => {
+    dispatch(restartTheGame())
+  }
   const closeModal = () => {
     dispatch(freezeTheGame())
   }
@@ -18,7 +20,7 @@ const ResetRestartModal = () => {
         <i className="close-icon fas fa-times-circle" onClick={() => closeModal()}></i>
         <p className="winner-title">{winnerName} win</p>
         <div>
-          <Button active={true} action={handleClose} text="New Game" playerId={0} allowedToClick={true} ></Button>
+          <Button active={true} action={resetTheGame} text="New Game" playerId={0} allowedToClick={true} ></Button>
           <Button active={true} action={closeModal} text="Exit" playerId={0} customStyle={style.exitButton} allowedToClick={true}></Button>
         </div>
       </section>
